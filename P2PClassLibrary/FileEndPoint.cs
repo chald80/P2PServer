f86c0e8c70656d1319d9b55aaaf5853ce3def903
chald80 <72771464+chald80@users.noreply.github.com>
2021-10-14 12:31:22 +0200

    update

--- P2PClassLibrary/FileEndPoint.cs
+++ P2PClassLibrary/FileEndPoint.cs
@@ -4,7 +4,9 @@ namespace P2PClassLibrary
 {
     public class FileEndPoint
     {
-
+        public string IpAddress { get; set; }
+        public string Port { get; set; }
+        public string Peer { get; set; }
 
     }
 }
